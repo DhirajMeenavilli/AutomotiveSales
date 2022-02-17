04ff6134afaa003b30ad4561b5ea8f089b8b32de
Dhiraj Meenavilli <meenavillid@gmail.com>
2022-02-16 18:10:04 -0700

    Split the data into training and testing data

--- SupervisedCarPrediction.py
+++ SupervisedCarPrediction.py
@@ -2,4 +2,5 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('/home/dhiraj/Documents/Code/AutomotiveSales/trainingData.csv')
-print(df)
+training_set = df[:3000]
+testing_set = df[3000:]
